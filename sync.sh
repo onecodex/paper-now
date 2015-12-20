c6df14a53e58001c534f0fcdda75e409696346c1
Nick Greenfield <nick@onecodex.com>
2015-12-20 09:45:39 -0800

    Exclude a private/ folders as needed (for scratch work etc.)

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
 set -e
 LOCATION=paper-now
 jekyll build
-cd _site && s3cmd sync --rexclude "^\." --cf-invalidate --cf-invalidate-default-index ./* . s3://science.onecodex.com/$LOCATION/
+cd _site && s3cmd sync --rexclude "^\." --rexclude "private/*" --cf-invalidate --cf-invalidate-default-index ./* . s3://science.onecodex.com/$LOCATION/
